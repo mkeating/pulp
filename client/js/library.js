204ed02758ea90d933b78e712ec4e41245c3ea03
Michael <michael@Michaels-MBP.fios-router.home>
2015-11-27 12:10:33 -0500

    begin restructure; some more panel styling

---
+++ client/js/library.js
@@ -0,0 +1,20 @@
+
+  Template.library.helpers({
+
+    stories: function() {
+      return Panels.find({origin: true}).fetch();
+    },  
+  })
+
+  Template.libraryStory.helpers({
+
+    truncatedText: function(text) {
+      
+      //TODO: use regex to clear all tags
+      var cleanTags = text.replace(/<span>/g, '');
+
+      cleanTags = cleanTags.replace(/<\/span>/g, '');
+      var truncatedText = cleanTags.substring(0, 500) + "...";
+      return truncatedText;
+    }
+  })
